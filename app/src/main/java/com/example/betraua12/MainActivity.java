484823cf01b92b57f3940a6e45acf051a4554661
resistencia1 <crisevil1@gmail.com>
2020-05-31 13:29:08 -0500

    con el share

--- app/src/main/java/com/example/betraua12/MainActivity.java
+++ app/src/main/java/com/example/betraua12/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.betraua12;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+
+import com.commonsware.cwac.provider.StreamProvider;
 
 import Adaptadores.GaleriaImagenesAdapter;
 
@@ -28,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         mPlayer4=MediaPlayer.create(MainActivity.this,R.raw.stronger);
         mPlayer5=MediaPlayer.create(MainActivity.this,R.raw.faster);
         mPlayer6=MediaPlayer.create(MainActivity.this,R.raw.workit);
-  
+
         gridViewImagenes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -52,6 +56,34 @@ public class MainActivity extends AppCompatActivity {
                         mPlayer6.start();
                         break;
                 }
+            }
+        });
+
+        gridViewImagenes.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+
+                        String COMMON_AUTHORITY = "com.yourpackage.AssetProvider";
+                        Uri uri = Uri.parse("content://" + COMMON_AUTHORITY)
+                                .buildUpon()
+                                .appendPath(StreamProvider.getUriPrefix(COMMON_AUTHORITY))
+                                .appendPath("our") //the name attribute
+                                .appendPath("our.mp3") //the path attribute, + extension
+                                .build();
+
+                        Intent share = new Intent(Intent.ACTION_SEND);
+                        share.setType("audio/mp3");
+                        share.putExtra(Intent.EXTRA_STREAM, uri);
+                        startActivity(Intent.createChooser(share, "Share Audio File"));
+
+
+
+                        //break;
+
+                }
+                return false;
             }
         });
 
